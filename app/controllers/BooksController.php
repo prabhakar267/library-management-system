047e3fed883a3eec675d5ceec9a12b54ee752f2b
Prabhakar Gupta <prabhakargupta267@gmail.com>
2015-12-17 00:21:57 +0530

    one book search response complete

--- app/controllers/BooksController.php
+++ app/controllers/BooksController.php
@@ -145,32 +145,55 @@ class BooksController extends \BaseController {
 	public function edit($id)
 	{	
 		$issue = Issue::find($id);
+		if($issue == NULL){
+			throw new Exception('Invalid Book ID');
+		}
+
 		$book = Books::find($issue->book_id);
 
 		$issue->book_name = $book->title;
 		$issue->author = $book->author;
-		// $issue->book = $book;
-
-		$issue->category = Categories::find($book->category_id)->category;
+		
+		$issue->category = Categories::find($book->category_id)
+			->category;
 
 		$issue->available_status = (bool)$issue->available_status;
 		if($issue->available_status == 1){
 			return $issue;
 		}
 		
-		// $book_list = $book_list->get();
+		$conditions = array(
+			'return_time'	=> 0,
+			'book_issue_id'	=> $id,
+		);
+		$book_issue_log = Logs::where($conditions)
+			->take(1)
+			->get();
 
-		// foreach($book_list as $book){
-			// $conditions = array(
-				// 'book_id'			=> $book->book_id,
-				// 'available_status'	=> 1
-			// );
-			
-			// $count = Issue::where($conditions)
-				// ->count();
+		foreach($book_issue_log as $log){
+			$student_id = $log->student_id;
+		}
 
-			// $book->avaliability = ($count > 0) ? true : false;
-		// }
+		$student_data = Student::find($student_id);
+
+		unset($student_data->email_id);
+		unset($student_data->books_issued);
+		unset($student_data->approved);
+		unset($student_data->rejected);
+
+		$student_branch = Branch::find($student_data->branch)
+			->branch;
+		$roll_num = $student_data->roll_num . '/' . $student_branch . '/' . substr($student_data->year, 2, 4);
+
+		unset($student_data->roll_num);
+		unset($student_data->branch);
+		unset($student_data->year);
+
+		$student_data->roll_num = $roll_num;
+
+		$student_data->category = StudentCategories::find($student_data->category)
+			->category;
+		$issue->student = $student_data;
 
         return $issue;
 	}
