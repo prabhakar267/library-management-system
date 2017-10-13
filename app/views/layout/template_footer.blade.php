14822e05b520280cd902699dabe419329f762fde
Gareth McCumskey <gareth@mccumskey.com>
2017-10-13 10:16:30 +0200

    Added dynamic year for copyright using blade syntax

--- app/views/layout/template_footer.blade.php
+++ app/views/layout/template_footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer">
 	<div class="container">
-		<b class="copyright">&copy; 2015 NSIT - Automated Library Management System </b> All rights reserved.
+		<b class="copyright">&copy; {{ date('Y') }} NSIT - Automated Library Management System </b> All rights reserved.
 	</div>
 </div>
