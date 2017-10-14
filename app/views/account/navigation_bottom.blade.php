6561424cd39702ee691709ea93a8ea3448f43ea7
Soumya Ranjan <soumya.cr17@gmail.com>
2017-10-14 09:41:44 +0300

    Update navigation_bottom.blade.php

--- app/views/account/navigation_bottom.blade.php
+++ app/views/account/navigation_bottom.blade.php
@@ -1,6 +1,6 @@
 <div class="footer">
 	<div class="container">
-		<b class="copyright">&copy; 2015 NSIT - Automated Library Management System </b> All rights reserved.
+		<b class="copyright">&copy; 2015 Automated Library Management System </b> All rights reserved.
 		<br><a href="https://github.com/prabhakar267/library-management-system" target="_blank">Fork on Github</a>
 	</div>
 </div>
