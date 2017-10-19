1ba8d939f360392c3e8407ed2f4cc12758b95e6b
harishb19 <harish.balasubramanian15@siesgst.ac.in>
2017-10-19 22:37:30 +0530

    Dynamic date added

--- app/views/account/navigation_bottom.blade.php
+++ app/views/account/navigation_bottom.blade.php
@@ -1,6 +1,12 @@
 <div class="footer">
 	<div class="container">
-		<b class="copyright">&copy; 2015 - Automated Library Management System </b> All rights reserved.
+		<b class="copyright">&copy; <span id="ccdate">2015</span> - Automated Library Management System </b> All rights reserved.
 		<br><a href="https://github.com/prabhakar267/library-management-system" target="_blank">Fork on Github</a>
 	</div>
 </div>
+
+<script type="text/javascript">
+    var d = new Date();
+    var n =  d.getFullYear();
+    document.getElementById("ccdate").innerHTML = n;
+</script>
